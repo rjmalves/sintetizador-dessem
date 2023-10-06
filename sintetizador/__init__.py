aaec4b4a2d1f2105d56a3aa11e56eff6ba48249a
marianasnoel <marianasimoesnoel@gmail.com>
2023-10-06 09:42:11 -0300

    define versao

---
+++ sintetizador/__init__.py
@@ -0,0 +1,10 @@
+"""
+sintetizador-dessem
+====================
+
+O sintetizador-dessem é um módulo em Python para
+padronização dos arquivos de saída do DESSEM em um
+modelo unificado de dados.
+"""
+
+__version__ = "0.0.1"
